66df84f396986905e20e6d72b3b82de1a2d9c9eb
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-12 13:04:44 -0300

    [IMP] project/install.py (ADD "clv_mass_editing" module installation)

--- project/install.py
+++ project/install.py
@@ -219,6 +219,9 @@ class CLVhealthJCAFB(object):
         # ]
         # install_upgrade_module('clv_off', upgrade, group_names)
 
+        group_names = []
+        self.install_upgrade_module('clv_mass_editing', self.upgrade_all, group_names)
+
         # group_names = [
         #     'User (File System)',
         #     'Manager (File System)',
