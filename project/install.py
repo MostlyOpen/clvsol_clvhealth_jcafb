6eb0f75f29107232ca641136de2f35fd7b5d7041
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-25 13:01:38 -0300

    [IMP] project/install.py (ADD "clv_community" module installation))

--- project/install.py
+++ project/install.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-# Copyright (C) 2016-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import images
 
@@ -333,12 +317,12 @@ class CLVhealthJCAFB(object):
         # group_names = []
         # install_upgrade_module('clv_animal_address_history', upgrade, group_names)
 
-        # group_names = [
-        #     'User (Community)',
-        #     'Manager (Community)',
-        #     'Super Manager (Community)',
-        # ]
-        # install_upgrade_module('clv_community', upgrade, group_names)
+        group_names = [
+            'User (Community)',
+            'Manager (Community)',
+            'Super Manager (Community)',
+        ]
+        self.install_upgrade_module('clv_community', self.upgrade_all, group_names)
 
         # group_names = []
         # install_upgrade_module('clv_community_history', upgrade, group_names)
