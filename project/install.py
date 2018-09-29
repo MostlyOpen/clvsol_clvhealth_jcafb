dd14851390bf232b0a4471c6843d3e34df86d284
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-29 17:54:51 -0300

    [IMP] project/install.py (ADD "clv_document_sync" module installation)

--- project/install.py
+++ project/install.py
@@ -384,6 +384,9 @@ class CLVhealthJCAFB(object):
         group_names = []
         self.install_upgrade_module('clv_document_history', self.upgrade_all, group_names)
 
+        group_names = []
+        self.install_upgrade_module('clv_document_sync', self.upgrade_all, group_names)
+
         # group_names = [
         #     'User (Document (Off))',
         #     'Manager (Document (Off))',
