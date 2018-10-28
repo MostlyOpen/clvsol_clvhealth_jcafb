cd6756235bad1ab128a26ffaf619d5020661f49a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-10-28 19:05:25 -0300

    [IMP] project/install.py (ADD "clv_phase_sync_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -238,6 +238,9 @@ class CLVhealthJCAFB(object):
         group_names = []
         self.install_upgrade_module('clv_phase_jcafb', self.upgrade_all, group_names)
 
+        group_names = []
+        self.install_upgrade_module('clv_phase_sync_jcafb', self.upgrade_all, group_names)
+
 
 if __name__ == '__main__':
 
