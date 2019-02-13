a4d4aeb8f06701a0f7dcefb43444b2f20c262322
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-13 14:00:06 -0200

    [IMP] project/install.py (ADD "clv_address_l10n_br" module installation)

--- project/install.py
+++ project/install.py
@@ -326,8 +326,8 @@ class CLVhealthJCAFB(object):
         group_names = []
         self.install_upgrade_module('clv_partner_entity_l10n_br', self.upgrade_all, group_names)
 
-        # # group_names = []
-        # # self.install_upgrade_module('clv_address_l10n_br', self.upgrade_all, group_names)
+        group_names = []
+        self.install_upgrade_module('clv_address_l10n_br', self.upgrade_all, group_names)
 
         # ############################################################################################
         #
