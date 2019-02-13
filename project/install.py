4b43058a2e4812c217648e977433b822f7e2ea31
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-13 18:21:48 -0200

    [IMP] project/install.py (REM "clv_family_sync_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -408,9 +408,6 @@ class CLVhealthJCAFB(object):
         self.install_upgrade_module('clv_person_sync_jcafb', self.upgrade_all, group_names)
 
         group_names = []
-        self.install_upgrade_module('clv_family_sync_jcafb', self.upgrade_all, group_names)
-
-        group_names = []
         self.install_upgrade_module('clv_employee_sync_jcafb', self.upgrade_all, group_names)
 
 
