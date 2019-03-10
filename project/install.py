f963ec1823381667f5d423be1b1133778a269922
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-10 14:13:06 -0300

    [IMP] project/install.py (ADD "clv_off" module installation)

--- project/install.py
+++ project/install.py
@@ -204,6 +204,8 @@ class CLVhealthJCAFB(object):
         # ]
         self.install_upgrade_module('clv_base', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_off', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_global_log', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_external_sync', self.upgrade_all, group_names)
