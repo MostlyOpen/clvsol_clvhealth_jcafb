0d28fb057587a3642ba4e807eb279b84a06efb4f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-14 14:41:33 -0300

    [IMP] project/install.py (ADD "clv_address_off" module installation)

--- project/install.py
+++ project/install.py
@@ -258,6 +258,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_address_history', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_address_off', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_family', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_family_history', self.upgrade_all, group_names)
