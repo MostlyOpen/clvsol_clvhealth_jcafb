4f4d81168ea8fdc4235a43374fce50c4694eb668
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-02 10:51:18 -0300

    [IMP] project/install.py (ADD "clv_mfile" module installation)

--- project/install.py
+++ project/install.py
@@ -236,6 +236,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_document_history', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_mfile', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_lab_test', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_lab_test_history', self.upgrade_all, group_names)
