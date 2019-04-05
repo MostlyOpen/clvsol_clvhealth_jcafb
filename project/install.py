d1bcc105223b32eb831e469183eddda349ab0955
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-05 18:05:04 -0300

    [IMP] project/install.py (ADD "clv_export_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -328,6 +328,8 @@ class CLVhealthJCAFB(object):
 
         # self.install_upgrade_module('clv_person_off_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_export_jcafb', self.upgrade_all, group_names)
+
         # ############################################################################################
         #
         # CLVsol Odoo Addons - JCAFB customizations (External Sync)
