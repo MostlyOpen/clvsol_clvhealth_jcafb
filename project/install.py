f22f8523cd22f24c1b608e1ec559ee1b5cec7946
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-11 14:23:44 -0300

    [IMP] project/install.py (ADD "clv_lab_test_export_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -322,6 +322,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_lab_test_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_lab_test_export_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_address_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_address_history_jcafb', self.upgrade_all, group_names)
