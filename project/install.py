c60ddf5475d51c757aa6f2c91402d579ef102ef4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-16 20:12:31 -0300

    [IMP] project/install.py (ADD "clv_person_export" module installation)

--- project/install.py
+++ project/install.py
@@ -272,6 +272,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_person_history', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_person_export', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_person_off', self.upgrade_all, group_names)
 
         # ############################################################################################
