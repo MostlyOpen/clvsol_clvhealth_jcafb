7f2a4e941d8d45329847e87658dc5c1efff0d819
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-18 18:12:07 -0300

    [IMP] project/install.py (ADD "clv_set" module installation)

--- project/install.py
+++ project/install.py
@@ -232,6 +232,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_survey_history', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_set', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_event', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_event_history', self.upgrade_all, group_names)
