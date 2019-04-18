19cfbce0d9fa01a99608c447a86ce04e59647e1b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-18 18:12:42 -0300

    [IMP] project/install.py (ADD "clv_set_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -326,6 +326,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_phase_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_set_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_event_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_community_jcafb', self.upgrade_all, group_names)
