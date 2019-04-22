37fbdc60147315368c7768f90a751d8615de97c6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-22 17:10:29 -0300

    [IMP] project/install.py (ADD "clv_person_sel_jcafb" module installation)

--- project/install.py
+++ project/install.py
@@ -360,6 +360,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_person_export_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_person_sel_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_person_off_jcafb', self.upgrade_all, group_names)
 
         # ############################################################################################
