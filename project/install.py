d1cee4b8fb46fea506dbf1f3a98a0c46f238cc7a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-05-29 15:53:13 -0300

    [IMP] project/install.py (Exclude "OCA/l10n-brazil" modules")

--- project/install.py
+++ project/install.py
@@ -167,18 +167,6 @@ class CLVhealthJCAFB(object):
 
         # ############################################################################################
         #
-        # OCA/l10n-brazil
-        #
-        # ############################################################################################
-
-        # self.install_upgrade_module('l10n_br_base', self.upgrade_all, group_names)
-
-        # self.install_upgrade_module('l10n_br_zip', self.upgrade_all, group_names)
-
-        # self.install_upgrade_module('l10n_br_zip_correios', self.upgrade_all, group_names)
-
-        # ############################################################################################
-        #
         # OCA/server-tools
         #
         # ############################################################################################
