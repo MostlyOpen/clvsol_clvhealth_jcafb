8af8fff81ea3c2ab9374ce7577388b256cfc2c4a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-06 20:36:15 -0300

    [IMP] project/install.py (Enable set modules)

--- project/install.py
+++ project/install.py
@@ -228,7 +228,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_survey_history', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_set', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_set', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_event', self.upgrade_all, group_names)
 
@@ -324,7 +324,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_employee_jcafb', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_set_jcafb', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_set_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_event_jcafb', self.upgrade_all, group_names)
 
