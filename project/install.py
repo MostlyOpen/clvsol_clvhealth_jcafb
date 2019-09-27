9205b1f8d000a6062fd3b2f80f114f413e64dcb2
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-27 15:06:29 -0300

    [IMP] project/install.py (ADD clv_address_aux_verification_jcafb module installation)

--- project/install.py
+++ project/install.py
@@ -350,6 +350,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_address_aux_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_address_aux_verification_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_family_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_family_history_jcafb', self.upgrade_all, group_names)
