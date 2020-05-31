1b83cf35263e4d6ea3b0fbe5d3cc8aa7879288d6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-05-31 15:20:20 -0300

    [IMP] project/install.py (Disabled clv_l10n_br_zip_correios module installation)

--- project/install.py
+++ project/install.py
@@ -187,7 +187,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_l10n_br_zip', self.upgrade_all, group_names)
 
-        self.install_upgrade_module('clv_l10n_br_zip_correios', self.upgrade_all, group_names)
+        # self.install_upgrade_module('clv_l10n_br_zip_correios', self.upgrade_all, group_names)
 
         # ############################################################################################
         #
