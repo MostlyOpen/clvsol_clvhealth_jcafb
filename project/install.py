8683fe853afa9e776b21420727561674a2dcb75e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-06-12 16:08:37 -0300

    [IMP] project/install.py (ADD (ADD clv_verification_sync_jcafb module installation) module installation)

--- project/install.py
+++ project/install.py
@@ -498,6 +498,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_base_sync_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_file_system_sync_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_global_tag_sync_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_phase_sync_jcafb', self.upgrade_all, group_names)
