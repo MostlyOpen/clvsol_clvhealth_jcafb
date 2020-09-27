af8d65e2fc65e8137b9f5e6721d43777aaa48eed
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-09-27 10:16:01 -0300

    [IMP] project/install.py (Enable clv_survey installation)

--- project/install.py
+++ project/install.py
@@ -202,7 +202,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_employee', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_survey', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_survey', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_event', self.upgrade_all, group_names)
 
