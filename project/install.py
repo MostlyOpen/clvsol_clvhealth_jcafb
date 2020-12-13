947f758f86eb82d102c004ed87532a3a65073e5e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-12-13 14:49:13 -0300

    [IMP] project/install.py (ADD clv_employee_summary_jcafb module installation)

--- project/install.py
+++ project/install.py
@@ -308,7 +308,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_summary_jcafb', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_employee_summary_jcafb', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_employee_summary_jcafb', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_address_summary_jcafb', self.upgrade_all, group_names)
 
