405d07a91816215d21f04ee2c565a3955beeef36
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-01-27 17:53:36 -0300

    [IMP] project/install.py (Disable clv_file_system_sync_jcafb installation)

--- project/install.py
+++ project/install.py
@@ -414,7 +414,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_base_sync_jcafb', self.upgrade_all, group_names)
 
-        self.install_upgrade_module('clv_file_system_sync_jcafb', self.upgrade_all, group_names)
+        # self.install_upgrade_module('clv_file_system_sync_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_phase_sync_jcafb', self.upgrade_all, group_names)
 
