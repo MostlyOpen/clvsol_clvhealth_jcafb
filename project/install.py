287c0c99d111a18e242c85933ee4e87d133f959c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-03-03 13:56:16 -0300

    [IMP] project/install.py (ADD clv_export module installation)

--- project/install.py
+++ project/install.py
@@ -340,7 +340,7 @@ class CLVhealthJCAFB(object):
         #
         # ############################################################################################
 
-        # self.install_upgrade_module('clv_export', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_export', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_document_export', self.upgrade_all, group_names)
 
