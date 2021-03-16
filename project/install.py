71e204ccd110975b5bc82c1eed93ab9b0f1e26aa
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-03-16 13:30:20 -0300

    [IMP] project/install.py

--- project/install.py
+++ project/install.py
@@ -222,13 +222,13 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_person_relation', self.upgrade_all, group_names)
 
-        self.install_upgrade_module('clv_patient', self.upgrade_all, group_names)
-
-        self.install_upgrade_module('clv_patient_community', self.upgrade_all, group_names)
-
         self.install_upgrade_module('clv_residence', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_residence_community', self.upgrade_all, group_names)
+
+        self.install_upgrade_module('clv_patient', self.upgrade_all, group_names)
+
+        self.install_upgrade_module('clv_patient_community', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_address_aux', self.upgrade_all, group_names)
 
