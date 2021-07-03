3befb8815e66e80111ad306297cdb7d15e97671a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-07-03 10:46:22 -0300

    [IMP] project/install.py (Disable clv_residence_sync_jcafb and clv_patient_sync_jcafb modules installation)

--- project/install.py
+++ project/install.py
@@ -482,9 +482,9 @@ class CLVhealthJCAFB(object):
 
         # self.install_upgrade_module('clv_person_aux_sync_jcafb', self.upgrade_all, group_names)
 
-        self.install_upgrade_module('clv_residence_sync_jcafb', self.upgrade_all, group_names)
+        # self.install_upgrade_module('clv_residence_sync_jcafb', self.upgrade_all, group_names)
 
-        self.install_upgrade_module('clv_patient_sync_jcafb', self.upgrade_all, group_names)
+        # self.install_upgrade_module('clv_patient_sync_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_event_sync_jcafb', self.upgrade_all, group_names)
 
