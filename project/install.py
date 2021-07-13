51bc3a6c52e4665b8e255006f5a43b35e78009c7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-07-13 13:36:09 -0300

    [IMP] project/install.py (ADD clv_residence_history module installation)

--- project/install.py
+++ project/install.py
@@ -224,6 +224,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_residence', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_residence_history', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_residence_community', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_patient', self.upgrade_all, group_names)
