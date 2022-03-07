b7f3d057191800da2670588ab77f27593d18ab3d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-07 11:31:41 -0300

    [IMP] project/install.py (Enable clv_global_log module installation)

--- project/install.py
+++ project/install.py
@@ -176,7 +176,7 @@ class CLVhealthJCAFB(object):
         # ]
         self.install_upgrade_module('clv_base', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_global_log', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_global_log', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_file_system', self.upgrade_all, group_names)
 
