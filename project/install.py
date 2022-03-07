e988253a7aac3ae58422567cab42353a2ac1b3ee
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-07 15:02:17 -0300

    [IMP] project/install.py (Enable clv_base_sync_jcafb module installation)

--- project/install.py
+++ project/install.py
@@ -464,7 +464,7 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_external_sync_jcafb', self.upgrade_all, group_names)
 
-        # self.install_upgrade_module('clv_base_sync_jcafb', self.upgrade_all, group_names)
+        self.install_upgrade_module('clv_base_sync_jcafb', self.upgrade_all, group_names)
 
         # self.install_upgrade_module('clv_file_system_sync_jcafb', self.upgrade_all, group_names)
 
