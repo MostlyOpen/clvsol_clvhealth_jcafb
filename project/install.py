8a35993edfa846d9b9052781162df0e9e0b52649
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-28 10:09:36 -0300

    [IMP] project/install.py

--- project/install.py
+++ project/install.py
@@ -530,6 +530,8 @@ class CLVhealthJCAFB(object):
 
         self.install_upgrade_module('clv_external_sync_jcafb', self.upgrade_all, group_names)
 
+        self.install_upgrade_module('clv_phase_sync_jcafb', self.upgrade_all, group_names)
+
         self.install_upgrade_module('clv_partner_entity_sync_jcafb', self.upgrade_all, group_names)
 
         self.install_upgrade_module('clv_residence_sync_jcafb', self.upgrade_all, group_names)
